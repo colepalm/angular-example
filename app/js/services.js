7660ee513e022c90b5841dba155cd9309fe74b02
Cole Palm <palm.cole@gmail.com>
2016-09-21 16:17:57 -0700

    further api link

--- app/js/services.js
+++ app/js/services.js
@@ -1,14 +1,28 @@
-angular.module('F1FeederApp.services', []).
-factory('ergastAPIservice', function($http) {
+angular.module('F1FeederApp.services', [])
+    .factory('ergastAPIservice', function($http) {
 
-    var ergastAPI = {};
+        var ergastAPI = {};
 
-    ergastAPI.getDrivers = function() {
-        return $http({
-            method: 'JSONP',
-            url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
-        });
-    }
+        ergastAPI.getDrivers = function() {
+            return $http({
+                method: 'JSONP',
+                url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+            });
+        }
 
-    return ergastAPI;
-});
+        ergastAPI.getDriverDetails = function(id) {
+            return $http({
+                method: 'JSONP',
+                url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
+            });
+        }
+
+        ergastAPI.getDriverRaces = function(id) {
+            return $http({
+                method: 'JSONP',
+                url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
+            });
+        }
+
+        return ergastAPI;
+    });
