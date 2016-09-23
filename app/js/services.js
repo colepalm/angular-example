5413169e50b4f4638e25ce7e443723831100cf04
Cole Palm <palm.cole@gmail.com>
2016-09-23 02:39:03 -0700

    update api call

--- app/js/services.js
+++ app/js/services.js
@@ -1,16 +1,18 @@
 angular.module('nflNewsApp.services', [])
-    .factory('ergastAPIservice', function($http) {
+    .factory('nflNewsAPIService', function($http) {
+        var nflAPI = {};
 
-        var ergastAPI = {};
-
-        ergastAPI.getDrivers = function() {
+        nflAPI.getCurrentScores = function(req) {
             return $http({
-                method: 'JSONP',
-                url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
-            });
+                method: 'GET',
+                url: 'https://api.fantasydata.net/v3/nfl/scores/JSON/AreAnyGamesInProgress',
+                headers: {
+                    'Ocp-Apim-Subscription-Key': '9a7b6708b29f49d581330ffaf2950696'
+                }
+            })
         }
 
-        ergastAPI.getDriverDetails = function(id) {
+        /*ergastAPI.getDriverDetails = function(id) {
             return $http({
                 method: 'JSONP',
                 url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
@@ -22,7 +24,7 @@ angular.module('nflNewsApp.services', [])
                 method: 'JSONP',
                 url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
             });
-        }
+        }*/
 
-        return ergastAPI;
+        return nflAPI;
     });
